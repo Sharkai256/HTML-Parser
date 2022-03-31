3dcee3292fb7d3a8f7b97acdeea32f95f3f4e037
UROBBYU <srobby228@gmail.com>
2022-03-31 19:21:02 +0300

    +Timer to test

--- test.ts
+++ test.ts
@@ -3,6 +3,8 @@ import parser = require('./index')
 
 const file = fs.readFileSync('tests/test1.html').toString()
 
+console.time('test')
 const dom = parser(file)
+console.timeEnd('test')
 
 console.log(dom)
