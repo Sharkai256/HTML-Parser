73520d7d2e5fb6f1932c6fbc5c8fca22f42776ef
Sharkai <justasharky@i.ua>
2022-04-12 21:40:15 +0300

    *Small changes*

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-//import serialize from './ser'
+import serialize from './ser'
 
 type Element = {
     tagName: string
@@ -7,22 +7,17 @@ type Element = {
 
 export interface SimpleDOM extends Element {
     title: string
+    //DOM here aswell
 }
 
-const string = '<h2>juice</h2>';
-
-//i should try something different than this
 const parser = (html: string): SimpleDOM => {
-    console.log(string);
+    //here
     return {
-        title: `${string.slice(string.indexOf('j'), string.lastIndexOf('e')+1)}`,
-        tagName: ``,
+        title: '',
+        tagName: '',
         children: []
     }
 }
 
-//when this would be working i will run tests
-const res = parser(string);
-console.log(res);
-//parser.serialize = serialize
-//export default parser
+parser.serialize = serialize
+export default parser
